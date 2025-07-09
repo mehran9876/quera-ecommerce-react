5b15e31f802cf271c92320053166d8fd15696c17
Nayira <nayira.yn@gmail.com>
2025-07-09 19:25:59 +0330

    QRE-84 zustand for _id

--- src/stores/use-auth-store.ts
+++ src/stores/use-auth-store.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 type AuthStore = {
   isAdmin: boolean;
+  userId: string | null;
   setIsAdmin: (value: boolean) => void;
+  setUserId: (id: string) => void;
 };
 
 export const useAuthStore = create<AuthStore>((set) => ({
   isAdmin: false,
+  userId: null,
   setIsAdmin: (value) => set({ isAdmin: value }),
+  setUserId: (id) => set({ userId: id }),
 }));
